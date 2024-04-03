4f57e2297fff4c10c412ce00f896e43b3a82808d
FELIPE TOSCANO MESSIAS <23.85167-8@maua.br>
2024-04-02 22:21:13 -0300

    first commit - aula 02/04/2024

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import CadastrarProduto from "./components/CadastrarProduto";
+import ConsultaCatalogo from "./components/ConsultaCatalogo";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          {/*//http://localhost:3000 */}
+          <Route path="/" element={<ConsultaCatalogo />}></Route>
+          {/*//http://localhost:3000/produtos*/}
+          <Route path="/produtos" element={<ConsultaCatalogo />}></Route>
+          {/*//http://localhost:3000/cad-produto*/}
+          <Route path="/cad-produto" element={<CadastrarProduto />}></Route>
+          {/*//http://localhost:3000/edit-produto/:id*/}
+          <Route path="/edit-produto/:id" element={<CadastrarProduto />}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
-
 export default App;
